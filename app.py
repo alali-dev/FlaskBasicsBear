38ca85e34eb94a00712b2e9ad2ac82e9f9061041
fiberesimaalalibo <fiberesimaalaliboralph@gmail.com>
2019-07-24 12:50:59 +0100

    commit 1
    Signed-off-by: fiberesimaalalibo <fiberesimaalaliboralph@gmail.com>

---
+++ app.py
@@ -0,0 +1,41 @@
+import json
+from flask import (Flask, render_template,
+                   redirect, url_for,request,make_response)
+from options import  DEFAULTS
+
+app = Flask(__name__)
+
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
+
+
+@app.route('/')
+def index():
+    data = get_saved_data()
+    return render_template("index.html",saves=data)
+
+@app.route('/builder')
+def builder():
+    return render_template('builder.html',
+    saves = get_saved_data(),
+    options = DEFAULTS)
+
+@app.route('/save',methods=['POST'])
+def save():
+#    import pdb; pdb.set_trace()
+    response = make_response(redirect(url_for('builder')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+
+#    response.set_cookie('character',json.dumps(dict(request.form.items())))
+
+    response.set_cookie('character',json.dumps(data))
+    return response
+#    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8000)
